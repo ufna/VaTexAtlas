91a91b66f775cb73d0f542fbfe7fd87eac96e76c
prudnikov <n.prudnikov@corp.mail.ru>
2017-12-18 12:24:37 +0300

    FlipX & FlipY for slate texture HUD draw

--- Source/VaTexAtlasPlugin/Classes/VtaPluginFunctionLibrary.h
+++ Source/VaTexAtlasPlugin/Classes/VtaPluginFunctionLibrary.h
@@ -15,7 +15,7 @@ class VATEXATLASPLUGIN_API UVtaPluginFunctionLibrary : public UBlueprintFunction
 public:
 	
 	UFUNCTION(BlueprintCallable, Category="HUD", meta=(DefaultToSelf = "Target", AdvancedDisplay = "6"))
-	static void DrawSlateTexture(class AHUD* Target, class UVtaSlateTexture* Texture, float ScreenX = 0.f, float ScreenY = 0.f, float ScreenW = 100.f, float ScreenH = 100.f, float TextureU = 0.f, float TextureV = 0.f, float TextureUWidth = 1.f, float TextureVHeight = 1.f, FLinearColor Tint = FLinearColor::White, EBlendMode BlendMode = BLEND_Translucent, float Scale = 1.f, bool bScalePosition = false, float Rotation = 0.f, FVector2D RotPivot = FVector2D::ZeroVector);
+	static void DrawSlateTexture(class AHUD* Target, class UVtaSlateTexture* Texture, float ScreenX = 0.f, float ScreenY = 0.f, float ScreenW = 100.f, float ScreenH = 100.f, float TextureU = 0.f, float TextureV = 0.f, float TextureUWidth = 1.f, float TextureVHeight = 1.f, FLinearColor Tint = FLinearColor::White, EBlendMode BlendMode = BLEND_Translucent, float Scale = 1.f, bool bScalePosition = false, float Rotation = 0.f, FVector2D RotPivot = FVector2D::ZeroVector, bool bFlipX = false, bool bFlipY = false);
 	
 	UFUNCTION(BlueprintPure, Category = "UMG")
 	static FSlateBrush CopyBrushWithSlateTexture(class UVtaSlateTexture* Texture, const FSlateBrush& SourceBrush);
