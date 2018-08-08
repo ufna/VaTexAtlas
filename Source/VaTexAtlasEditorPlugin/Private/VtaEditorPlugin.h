db1d15b23b9ed3f0405cc53bd3302a0183b7302e
v.alyamkin <ufna@ufna.ru>
2018-08-08 20:19:01 +0300

    Minor includes fix

--- Source/VaTexAtlasEditorPlugin/Private/VtaEditorPlugin.h
+++ Source/VaTexAtlasEditorPlugin/Private/VtaEditorPlugin.h
@@ -13,11 +13,10 @@
 
 #include "AssetToolsModule.h"
 #include "AssetRegistryModule.h"
-#include "Json.h"
 
 // You should place include statements to your module's private header files here.  You only need to
 // add includes for headers that are used in most of your module's source files though.
-#include "ModuleManager.h"
+#include "Modules/ModuleManager.h"
 
 DECLARE_LOG_CATEGORY_EXTERN(LogVaTexAtlasEditor, Log, All);
 
